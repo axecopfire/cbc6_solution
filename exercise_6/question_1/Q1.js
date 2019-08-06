7624c34fa43221dd787e7d92e3cdd00516b78885
HBull5 <51968637+HBull5@users.noreply.github.com>
2019-08-06 11:05:36 -0400

    Add files via upload

---
+++ exercise_6/question_1/Q1.js
@@ -0,0 +1,5 @@
+var kwikEMart = ["beef","chicken","pork","brocoli","carrots","taters","milk","eggs","cheese","hawt sawce"];
+
+for(var i = 0; i < kwikEMart.length; i++){
+	console.log(kwikEMart[i]);
+};
---
+++ exercise_6/question_3/Q3.js
@@ -0,0 +1,5 @@
+var strang = "jackknives";
+
+for(var i = 0; i < strang.length; i++){
+	console.log(strang[i].toUpperCase());
+}
